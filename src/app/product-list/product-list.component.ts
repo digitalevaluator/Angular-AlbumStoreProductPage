c70c032664d236f17b69a722e0f8f1e2d707bc7e
digitalevaluator <digitalevaluator@gmail.com>
2020-05-12 04:20:41 +0530

    added product listing changes

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
-import {ProductService} from '../product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -8,11 +8,9 @@ import {ProductService} from '../product.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
-  constructor(private _productService:ProductService) { }
-  products:Product[];
+  products: Product[];
+  constructor(private _productService: ProductService) { }
   ngOnInit() {
-    this._productService.getProducts().subscribe( response => this.products = response)
+    this._productService.getProducts().subscribe(response => this.products = response)
   }
-
 }
